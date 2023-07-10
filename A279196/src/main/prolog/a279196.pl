2cf211314c855452c234d99a564d8442e29d21f1
Luc <luc_rousseau@hotmail.com>
2023-07-10 17:22:14 +0200

    Memoization for better results

--- A279196/src/main/prolog/a279196.pl
+++ A279196/src/main/prolog/a279196.pl
@@ -1,85 +1,79 @@
-:- dynamic(dynamic_count/2).
-
 %===============================================================================
-% count_successes(+Goal, +Key, -Successes)
-% Tries to prove Goal as many times as possible
-% and counts its number of success Successes.
-% Side effect: uses assert/retract on dynamic predicate dynamic_count/2
-% in which +Key is used as the first argument to remember what is counted,
-% whereas the second argument is the count, proper.
-% This makes it possible to use count_successes/3 for more than one count.
-
+:- dynamic(dynamic_count/2).
+:- dynamic(memoized_v/3).
+:- dynamic(sum/3).
+clean :-
+	retractall(dynamic_count(_, _)),
+	retractall(memoized_v(_, _, _)),
+	retractall(sum(_, _, _)).
 count_successes(Goal, Key, Successes) :-
 	retractall(dynamic_count(Key, _)),
 	assert(dynamic_count(Key, 0)),
-	forall(Goal,
-		(
+	forall(Goal, (
 		dynamic_count(Key, N),
 		retract(dynamic_count(Key, N)),
 		NN is N + 1,
 		assert(dynamic_count(Key, NN))
-		)
-	),
+	)),
 	dynamic_count(Key, Successes).
-
+%===============================================================================
 a(N, AN) :-
 	M is N - 1,
-	count_successes(valid_chain_of_successors([1], M, _), a, AN).
-
+	v([1], M, AN).
 %===============================================================================
-% valid_chain_of_successors(+Q, +RemainingAmount, -Chain)
-% Chain is a valid list [Q1, Q2, ... Qn]
-% such that Q1 is a valid successor of Q,
-%           Q2 is a valid successor of Q1,
-% ...       Qn is a valid successor of Qn-1,
-% and RemainingAmount is totally consumed.
-
-
-valid_chain_of_successors(_Q, 0, []).
-valid_chain_of_successors(Q, RemainingAmount, Chain) :-
-	not(RemainingAmount = 0),
-	valid_successor(Q, RemainingAmount, QQ),
-	not((Q = [], QQ = [])),
-	amount_consumption(QQ, C),
-	NewRemainingAmount is RemainingAmount - C,
-	(
-		NewRemainingAmount = 0
-	->
-		Chain = [QQ]
-	;
+v(_Q, M, VQM) :-
+	M = 0, !, VQM = 1.
+v(Q, M, VQM) :-
+	memoized_v(Q, M, VQM), !.
+v(Q, M, VQM) :-
+	Q = [0 | QT], !,
+	v(QT, M, VQM).
+v(Q, M, VQM) :-
+	not(M = 0),
+	not(memoized_v(Q, M, VQM)),
+	Q = [QH | _QT],
+	not(QH = 0),
+	retractall(sum(Q, M, _)),
+	assert(sum(Q, M, 0)),
+	forall(
 		(
-		valid_chain_of_successors(QQ, NewRemainingAmount, SubChain),
-		Chain = [QQ | SubChain]
+			not(Q = []),
+			valid_successor(Q, M, QQ),
+			not(QQ = [])
+		),
+		(
+			amount_consumption(QQ, C),
+			MM is M - C,
+			v(QQ, MM, VQQMM),
+			sum(Q, M, Sum),
+			NewSum is Sum + VQQMM,
+			retract(sum(Q, M, Sum)),
+			assert(sum(Q, M, NewSum))
 		)
-	).
+	),
+	sum(Q, M, VQM),
+	assert(memoized_v(Q, M, VQM)).
 
 %===============================================================================
-% valid_successor(Q, RemainingAmount, QQ).
-% -QQ is a valid successor of +Q and has amount consumption at most +RemainingAmount.
 valid_successor(Q, RemainingAmount, QQ) :-
 	resize(Q, RemainingAmount, Q0),
 	InitialUpperRightSummand = 0,
 	instanciate(Q0, InitialUpperRightSummand, RemainingAmount, QQ0),
-	trim_right(QQ0, QQ).
+	trim_right(QQ0, QQ),
+	not(QQ = []).
 %===============================================================================
 repl(X, N, L) :-
     length(L, N),
     maplist(=(X), L).
-
 resize(Q, N, QQ) :-
-	repl(0, N, L),
+	M is max(0, N - 1),
+	repl(0, M, L),
 	append(Q, L, QQ).
-%===============================================================================
-% remove trailing zeros
-trim_right(Q, QQ) :-
-	reverse(Q, Q0),
-	trim_left(Q0, QQ0),
-	reverse(QQ0, QQ).
+trim_right(Q, QQ) :- reverse(Q, Q0), trim_left(Q0, QQ0), reverse(QQ0, QQ).
 trim_left([], []).
 trim_left(X, Y) :- X = [0 | XX], trim_left(XX, Y).
 trim_left(X, Y) :- X = [H | _], not(H = 0), Y = X.
 %===============================================================================
-%
 %   Q  *   UpperRightSummand
 %     / \ /
 %    *   *
@@ -97,13 +91,6 @@ instanciate([Q | Qs], UpperRightSummand, RemainingAmount, [QQ | QQs]) :-
 	instanciate(Qs, NewUpperRightSummand, NewRemainingAmount, QQs).
 %===============================================================================
 amount_consumption([], 0).
-amount_consumption([Q | Qs], C) :-
-	amount_consumption(Qs, CC),
-	C is CC + Q.
+amount_consumption([Q | Qs], C) :- amount_consumption(Qs, CC), C is CC + Q.
 %===============================================================================
-
-
-% ?- dict_create(D, toto, []), put_dict(key, D, value, DD), put_dict(key2, DD, value2, DDD).
-% D = toto{},
-% DD = toto{key:value},
-% DDD = toto{key:value, key2:value2}.
+% ?- dict_create(D, toto, []), put_dict(key, D, value, DD).
