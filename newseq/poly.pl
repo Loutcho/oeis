6b80057e9b2c6e9c5abba191e0360256bdde9848
Luc <luc_rousseau@hotmail.com>
2025-10-18 21:17:36 +0200

    Refacto

--- newseq/poly.pl
+++ newseq/poly.pl
@@ -1,4 +1,15 @@
-:- dynamic is_in_S/2.
+%
+% Let us define the set S thus:
+% (i) 1 is in S
+% (ii) P is in S implies 1 + x*P is in S
+% (iii) ((P is in S) and (Q is in S)) implies P*Q is in S
+% (iv) if the repeated application of (i), (ii), (iii) fails to prove
+%      that P is in S then P is not in S.
+%
+% Let us define a(n) as the number of P in S such that P(1) = n.
+%
+
+:- dynamic is_in_S/3.
 :- dynamic deferred_computation/2.
 
 main(Nmax) :-
@@ -7,28 +18,28 @@ main(Nmax) :-
 	export_data('data.txt', 1, Nmax),
 	export_bfile('bfile.txt', 1, Nmax).
 
+init :-
+	retractall(is_in_S(_, _, _)),
+	retractall(deferred_computation(_, _)).
+
 compute(Nmax) :-
-	assert_is_in_S([1,1], Nmax),
+	assert_is_in_S([1,1], Nmax, 2),
 	N = 3,
 	process_deferred_computations_up_to(Nmax, N).
 
-init :-
-	retractall(is_in_S(_, _)),
-	retractall(deferred_computation(_, _)).
-
-assert_is_in_S(P, Nmax) :-
-	is_in_S(P, PP1) % Guard against already known results
+assert_is_in_S(P, Nmax, Pedigree) :-
+	is_in_S(P, _PP1, AnotherPedigree) % Guard against already known results
 	->
-		maplist(write, ['Already known: ', P, ' with a P1 = ', PP1, ' is in S\n'])
+		maplist(write, [Pedigree , ' = ', P, ' comes to the same as: ', AnotherPedigree, ' = ', P, '\n'])
 	;
 	(
 		evaluation_at_1(P, P1),
-		assert(is_in_S(P, P1)),
+		assert(is_in_S(P, P1, Pedigree)),
 		E1 is P1 + 1,
 		(E1 =< Nmax -> assert(deferred_computation(sigma(P), E1)) ; true),
-		forall(is_in_S(Q, Q1), (
+		forall(is_in_S(Q, Q1, _), (
 			E2 is P1 * Q1,
-			(E2 =< Nmax -> assert(deferred_computation(P * Q, E2)) ; true)
+			(E2 =< Nmax -> assert(deferred_computation(Q * P, E2)) ; true)
 		))
 	).
 
@@ -60,20 +71,32 @@ process_deferred_computations_of_the_sigma_type(N, Nmax) :-
 	maplist(write, ['sigma(P):', CountSigma, '\n']),
 	forall(deferred_computation(sigma(P), N),
 	(
-		retract(deferred_computation(sigma(P), N)),
 		successor(P, Q),
-		(is_in_S(Q, _Q1) -> maplist(write, ['Unbelievable, ', Q, ' is already in S!\n']) ; assert_is_in_S(Q, Nmax))
-	)).
+		is_in_S(P, _, PedigreeP),
+		add_1_to_pedigree(PedigreeP, PedigreeQ),
+		assert_is_in_S(Q, Nmax, PedigreeQ)
+	)),
+	retractall(deferred_computation(sigma(_), N)).
+
+add_1_to_pedigree(N, NN) :-
+	number(N),
+	NN is N + 1.
+add_1_to_pedigree(N + Something, NN + Something) :-
+	number(N),
+	NN is N + 1.
+add_1_to_pedigree(P * Q, 1 + P * Q).
 
 process_deferred_computations_of_the_product_type(N, Nmax) :-
 	aggregate_all(count, deferred_computation(_ * _, N), CountProduct),
 	maplist(write, ['P*Q: ', CountProduct, '\n']),
 	forall(deferred_computation(P * Q, N),
 	(
-		retract(deferred_computation(P * Q, N)),
 		product(P, Q, PQ),
-		assert_is_in_S(PQ, Nmax)
-	)).
+		is_in_S(P, _, PedigreeP),
+		is_in_S(Q, _, PedigreeQ),
+		assert_is_in_S(PQ, Nmax, PedigreeP * PedigreeQ)
+	)),
+	retractall(deferred_computation(_ * _, N)).
 
 successor(P, [1 | P]).
  
@@ -125,4 +148,19 @@ export_bfile(Filename, Nmin, Nmax) :-
 a(1, 1).
 a(N, AN) :-
 	N > 1,
-	aggregate_all(count, is_in_S(_, N), AN).
+	aggregate_all(count, is_in_S(_, N, _), AN).
+
+delta_a(N, BN) :-
+	a(N, AN),
+	M is N - 1,
+	a(M, AM),
+	BN is AN - AM.
+
+% 18 is the first N such that a(N) is not equal to A130841(N). Why?
+of_interest_18 :-
+	 tell('18.txt'),
+	 forall(
+		is_in_S(P, 18, Pedigree),
+		writeln(P = Pedigree)
+	),
+	told.
