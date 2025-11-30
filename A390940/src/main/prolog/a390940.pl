21326add11cb0233d5706b7101853a2370050b6a
Luc <luc_rousseau@hotmail.com>
2025-11-30 22:08:07 +0100

    Create graphviz visualization of A390940

--- A390940/src/main/prolog/a390940.pl
+++ A390940/src/main/prolog/a390940.pl
@@ -8,7 +8,8 @@ t(J, TJ) :-
 	append(TI, UI, TIUI),
 	sort(1, @=<, TIUI, TIUIs),
 	group_pairs_by_key(TIUIs, KVs),
-	maplist(sum_values, KVs, TJ).
+	maplist(sum_values, KVs, TJ),
+	graphviz_cluster(J, TI, TJ).
 
 sum_values(K - Values, K - SumOfValues) :-
 	sum(Values, SumOfValues).
@@ -18,3 +19,72 @@ sum(List, Sum) :-
 
 add(X, Y, Z) :-
 	Z is X + Y.
+
+diffs(J) :-
+	t(J, TJ),
+	I is J - 1,
+	t(I, TI),
+	ord_subtract(TI, TJ, C),
+	ord_subtract(TJ, TI, D),
+	writeln('------------------------' - disappear),
+	maplist(writeln, C),
+	writeln('------------------------' - appear),
+	maplist(writeln, D).
+
+rgb_color(N, R, G, B) :-
+	R is (N * 7 + 3) mod 255,
+	G is (N * 13 + 5) mod 255,
+	B is (N * 17 + 8) mod 255.
+
+graphviz_cluster(J, TI, TJ) :-
+	maplist(write, ['subgraph cluster_', J, '{\n']),
+	maplist(write, ['    label = "j = ', J, '";\n']),
+	forall(
+		member(K - VJ, TJ),
+		graphviz_node(J, K, VJ, TI)
+	),
+	maplist(write, ['}\n']),
+	I is J - 1,
+	forall(
+		member(K - VJ, TJ),
+		graphviz_edge(J, K, VJ, I, TI)
+	).
+
+node_name(J, K, V, Name) :-
+	atomic_list_concat([J, ': T[', K, '] = ', V], Name).
+
+node_color(VI, VJ, '#777777') :-
+	VI = VJ.
+node_color(VI, VJ, '#0077FF') :-
+	not(VI = VJ).
+
+graphviz_node(J, K, VJ, TI) :-
+	node_name(J, K, VJ, NameJ),
+	(
+		member(K - VI, TI)
+	->
+		node_color(VI, VJ, Color)
+	;
+		Color = '#00FF00'
+	),
+	maplist(write, ['    "', NameJ, '" [fillcolor = "', Color, '"];\n']).
+
+graphviz_edge(J, K, VJ, I, TI) :-
+	member(K - VI, TI)
+	->
+	(
+		node_name(I, K, VI, NameI),
+		node_name(J, K, VJ, NameJ),
+		maplist(write, ['"', NameI, '" -> "', NameJ, '";\n'])
+	)
+	;
+	true.
+
+main :-
+	tell('toto.gv'),
+	maplist(write, ['digraph G {\n']),
+	maplist(write, ['    rankdir = "LR";\n']),
+	maplist(write, ['    node [shape = "box" style = "filled"];\n']),
+	t(80, _T),
+	maplist(write, ['}\n']),
+	told.
